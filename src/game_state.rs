9c73c2c053d9621c210f1428598c7bf0491e620b
Garrett Hunyadi <garrett.hunyadi@endurance.com>
2020-01-17 17:03:36 -0500

    bad state: need to refactor Room->Location+Rooms or similar

---
+++ src/game_state.rs
@@ -0,0 +1,72 @@
+use crate::rooms::{Room, Rooms};
+use crate::s;
+use rand::prelude::ThreadRng;
+use rand::seq::SliceRandom;
+use std::collections::HashMap;
+
+pub struct GameState<'a> {
+    rooms: Rooms<'a>,
+    map: MapData<'a>,
+    rng: ThreadRng,
+}
+
+impl<'a> GameState<'a> {
+    pub fn create_random() -> GameState<'a> {
+        let mut rooms = Rooms::new();
+        rooms.add(Room::new("lawn", "The Front lawn"));
+        rooms.add(Room::new("porch", "The Porch"));
+        rooms.add(Room::new("driveway", "The Driveway"));
+
+        let mut map = MapData::new();
+        map.insert(s!["lawn"], vec![s!("driveway"), s!("porch")]);
+        map.insert(s!["driveway"], vec![s!("lawn"), s!("porch")]);
+        map.insert(s!["porch"], vec![s!("driveway")]);
+
+        GameState {
+            rooms,
+            map,
+            rng: rand::thread_rng(),
+        }
+    }
+
+    pub fn start_at(&self) -> Option<&Room> {
+        match self.map.keys().nth(0) {
+            Some(room_id) => self.rooms.find(room_id),
+            None => None,
+        }
+    }
+
+    pub fn rand_room(&mut self, from: &String) -> Option<&Room> {
+        let options = self.map.get(from).unwrap();
+        if let Some(room_id) = options.choose(&mut self.rng) {
+            self.rooms.find(room_id)
+        } else {
+            None
+        }
+    }
+}
+type MapData<'a> = HashMap<String, Vec<String>>;
+
+// pub fn create_rooms() -> Vec<Room> {
+//     vec![
+//         Room {
+//             desc: s!("Front lawn"),
+//         },
+//         Room { desc: s!("Porch") },
+//         Room {
+//             desc: s!("Driveway"),
+//         },
+//     ]
+// }
+
+// fn create_map(rooms: &[Room]) -> MapData {
+//     let r1 = rooms.get(0).unwrap();
+//     let r2 = rooms.get(1).unwrap();
+//     let r3 = rooms.get(2).unwrap();
+
+//     let mut map = HashMap::new();
+//     map.insert(r1, vec![r2, r3]);
+//     map.insert(r2, vec![r1, r3]);
+//     map.insert(r3, vec![r1]);
+//     map
+// }
